87e7c4ae5eafd7a7443fcd6dfd151d5386a8bf78
sepwizzz <felipeeeeeeeeeee1212@gmail.com>
2024-11-19 06:59:31 -0500

    19/11/24-2

--- p_horizontal/settings.py
+++ p_horizontal/settings.py
@@ -138,11 +138,7 @@ STATICFILES_DIRS=[
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles' )
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
