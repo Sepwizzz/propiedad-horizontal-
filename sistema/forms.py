b4a5aaad8c32366784312c2137bdb186f715d754
Sepwizzz <115958805+Sepwizzz@users.noreply.github.com>
2024-12-30 00:12:22 -0500

    Update forms.py

--- sistema/forms.py
+++ sistema/forms.py
@@ -27,13 +27,6 @@ class ParqueaderoForm(forms.ModelForm):
         model = Parqueadero
         fields = [ 'tipo', 'placa_vehiculo','contacto','casa']
 
- 
-        
-    def clean_contacto(self):
-        contacto = self.cleaned_data.get('contacto')
-        if not contacto:
-            raise forms.ValidationError("El campo 'Contacto' es obligatorio.")
-        return contacto
 
     def clean_placa_vehiculo(self):
         placa = self.cleaned_data.get('placa_vehiculo')
